85804046c873daf889dcb2f2b81977665ec4ee34
RSC Application Delivery <dg-applicationdelivery@rsc.org.uk>
2023-03-04 17:28:18 +0000

    Image placeholder component working. Home view complete

--- src/app/image-placeholder/image-placeholder.component.ts
+++ src/app/image-placeholder/image-placeholder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sw-image-placeholder',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-placeholder.component.scss']
 })
 export class ImagePlaceholderComponent {
+    private _height: number = 100;
+    private _width: number = 100;
 
+    @Input() set height(value: number) {
+      this._height = value;
+    }
+
+    get height(): number {
+      return this._height;
+    }
+
+    @Input() set width(value: number) {
+      this._width = value;
+    }
+
+    get width(): number {
+      return this._width;
+    }
 }
